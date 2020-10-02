4304f7876bc58b10a4f40236e72fbd416170b98d
numdar335 <56404005+numdar335@users.noreply.github.com>
2020-10-02 18:37:33 +0300

    Add files via upload

---
+++ herman.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout << fixed;
+  int R;
+  cin >> R;
+  cout << M_PI*R*R << "\n";
+  cout << 2*R*R << "\n";
+}
