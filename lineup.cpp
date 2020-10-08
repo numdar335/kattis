e004b8aa3decfc494593817d69562b45b67e5901
numdar335 <56404005+numdar335@users.noreply.github.com>
2020-10-08 20:03:36 +0300

    Add files via upload

---
+++ lineup.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int N, i;
+  string A[20];
+  bool i_flag = true, d_flag = true;
+  cin >> N;
+  for (i = 1; i <= N; ++i) cin >> A[i-1];
+  for (i = 2; i <= N; ++i)
+    if (A[i-2] > A[i-1]) i_flag = false;
+    else if (A[i-2] < A[i-1]) d_flag = false;
+  if (i_flag == true) cout << "INCREASING\n";
+  else if (d_flag == true) cout << "DECREASING\n";
+  else cout << "NEITHER\n";
+  return 0;
+}
